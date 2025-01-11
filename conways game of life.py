cbd069645d0dccabc8e0719f1fc0f99aee1dcb12
Asmae ‎ <asmae@Asmaes-MacBook-Air.local>
2025-01-11 16:31:26 +0100

    Add get_next_grid function to compute the next state of the grid based on conway's game of life rules

--- conways game of life.py
+++ conways game of life.py
@@ -5,3 +5,7 @@ import time
 def count_neighbors(grid, row, col):
     area = grid[row-1:row+2, col-1:col+2]
     return np.sum(area) - grid[row, col]
+
+def get_next_grid(grid):
+    grid_with_border = np.pad(grid, 1, mode="constant")
+    new_grid = np.zeros_like(grid)
